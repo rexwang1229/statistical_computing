134ba30893e712fc657a1b9cfe0f5e948828df40
Rex Wang <rexwang1229@gmail.com>
2026-09-21 20:49:45 +0800

    hw1 q3 and q4 update

--- hw1.R
+++ hw1.R
@@ -24,6 +24,35 @@ hist(x, breaks = 100, freq = F, xlab = "x", ylab = "pdf f(x)",
 y <- seq(0, 1, 0.001)
 lines(y, dbeta(y, 3, 2))
 
+# 2
+# f(x) = (2 / (pi*R^2)) * sqrt(R^2 - x^2)
+# g(x) = 1 / 2R
+# f(x) / g(x) = (4 / (pi * R)) * sqrt(R^2 - x^2)
+# f(x) / g(x) has the largest value at 4/pi when x = 0
+# c = 4 / pi
+semicircle.sim <- function(R) {
+    while(TRUE) {
+         y <- runif(1, -R, R)
+         d <- (4 / (pi * R)) * sqrt(R^2 - y^2)
+         u <- runif(1, 0, 4/pi)
+         if(u < d) return(y)
+    }
+}
+
+dsemicircle <- function(R, x) {
+    (2 / (pi*R^2)) * sqrt(R^2 - x^2)
+}
+
+n <- 1000
+x <- rep(0, n)
+R <- 10
+for(i in 1:n) {
+    x[i] <- semicircle.sim(R)
+}
+hist(x, freq = F, xlab = "x", ylab = "pdf f(x)",
+    main = "theoretical and simulated semicircle density")
+y <- seq(min(x), max(x), 0.1)
+lines(y, dsemicircle(R = 10, x = y))
 
 # 3
 # cdf: 1 - (a / (x + a))^b
@@ -44,3 +73,39 @@ hist(y, breaks = 50, freq = F, xlab = "x", ylab = "pdf f(x)",
 dpareto <- function(x, a, b) b * a^b / (x + a)^(b + 1)
 z <- seq(0, 15, 0.01)
 lines(z, dpareto(z, 2, 4))
+
+# 4
+library(MASS)
+rwhis.general <- function(n, Sigma) {
+    x <- mvrnorm(n, mu, sigma)
+    M <- t(x) %*% x
+    M
+}
+rwhis.bartlett <- function(d, sigma) {
+    A <- matrix(rnorm(d * d), nrow = d, ncol = d)
+    A[!lower.tri(A)] <- 0
+    for(i in 1:d) {
+        diag(A)[i] <- sqrt(rchisq(1, df = n - i + 1))
+    }
+    L <- t(chol(sigma))
+    M <- L %*% A %*% t(A) %*% t(L)
+
+}
+
+n <- 100 # sample size
+d <- 2 # dimension
+N <- 2000 # iterations
+sigma <- matrix(c(1, 0.5, 0.5, 2), 2,2, byrow = T)
+mu <- numeric(d)
+
+# general
+set.seed(100)
+system.time(for(i in 1:N){
+    rwhis.general(n, Sigma)
+})
+
+# Bartlett
+set.seed(100)
+system.time(for(i in 1:N){
+    rwhis.bartlett(d, Sigma)
+})
